45866e7ac8147fb0911f40470f83ca3c09cf2a7d
Nina Herrmann <n_herr03@uni-muenster.de>
2016-12-02 09:54:14 +0100

    ci testing continous

--- classes/archiveduser.php
+++ classes/archiveduser.php
@@ -1,9 +1,24 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 /**
- * Created by IntelliJ IDEA.
- * User: nina
- * Date: 15.11.16
- * Time: 17:46
+ * Class archive user.
+ *
+ * @package   tool_deprovisionuser
+ * @copyright 2016 N. Herrmann
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class archiveduser {
 
