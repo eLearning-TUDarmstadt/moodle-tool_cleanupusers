d39dda0779b085943e85df947997109d2ac4d15d
Nina Herrmann <n_herr03@uni-muenster.de>
2017-01-05 15:13:45 +0100

    code style and precise description of settings

--- userstatus/timechecker/settings.php
+++ userstatus/timechecker/settings.php
@@ -25,7 +25,7 @@ defined('MOODLE_INTERNAL') || die();
 if ($hassiteconfig) {
     $url = $CFG->wwwroot . '/' . $CFG->admin . '/tool/deprovisionuser/timechecker/index.php';
     $settings->add(new admin_setting_configtext('userstatus_timechecker/suspendtime', get_string('suspendtime', 'tool_deprovisionuser'),
-        get_string('suspendtime', 'tool_deprovisionuser'), 90, PARAM_INT));
+        get_string('timechecker_time_to_archive', 'userstatus_timechecker'), 90, PARAM_INT));
     $settings->add(new admin_setting_configtext('userstatus_timechecker/deletetime', get_string('deletetime', 'tool_deprovisionuser'),
         get_string('timechecker_time_to_delete', 'userstatus_timechecker'), 365, PARAM_INT));
 }
