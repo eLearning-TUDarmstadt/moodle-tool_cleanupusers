71f72b6d9dd7c382b0f21d236f7599649394d26b
Nina Herrmann <n_herr03@uni-muenster.de>
2017-04-11 15:56:53 +0200

    improved code style settings php

--- settings.php
+++ settings.php
@@ -18,18 +18,18 @@
  * Adds tool_deprovisionuser link in admin tree
  *
  * @package    tool_deprovisionuser
- * @copyright  2016 N Herrmann
+ * @copyright  2016/17 N Herrmann
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 if ($hassiteconfig) {
     $url = $CFG->wwwroot . '/' . $CFG->admin . '/tool/deprovisionuser/index.php';
-    $ADMIN->add('users', new admin_externalpage(
-        'deprovisionuser',
+    $ADMIN->add('users', new admin_externalpage('deprovisionuser',
         get_string('plugintitel', 'tool_deprovisionuser'),
         "$CFG->wwwroot/$CFG->admin/tool/deprovisionuser/index.php"
     ));
+    // Adds an entry for every subplugin with an settings.php.
     $ADMIN->add('users', new admin_category('subplugins', get_string('subpluginsof', 'tool_deprovisionuser')));
     foreach (core_plugin_manager::instance()->get_plugins_of_type('userstatus') as $plugin) {
         global $CFG;
